174130dae9481e3a7e6987b0600df1c4e7b0d92a
Mistrcrx <84770090+Mistrcrx@users.noreply.github.com>
2023-11-01 13:21:33 +0100

    Small changes

--- resources/views/pages/projects.blade.php
+++ resources/views/pages/projects.blade.php
@@ -1,9 +1,6 @@
 @extends('layouts.app')
 
-
 @section('content')
-
- 
 
     {{-- Validatie/Erorhandling wordt gedaan in ProjectRequest.php --}}
     <div class="sub__pages">
@@ -17,7 +14,7 @@
         <div class="grid__wrapper">
             <fieldset class="fields base">
                 <h3>{{ __('Algemeen') }}</h3>
-                <div class="field">
+                <div class="field field-alt">
                     <label for="project_code">{{ __('Projectcode') }}</label>
                     <input id="" type="text" name="project_code" value="{{ old('project_code') }}">
                     @error('project_code')
@@ -27,7 +24,7 @@
                      @enderror
                 </div>
 
-                <div class="field">
+                <div class="field field-alt">
                     <label for="format">{{ __('Formaat') }}</label>
                    <input id="" type="text" name="format" value="{{ old('format') }}">
                     @error('format')
@@ -37,7 +34,7 @@
                     @enderror
                 </div>
 
-                <div class="field">
+                <div class="field field-alt">
                     <label for="release_name">{{ __('Uitgave') }}</label>
                    <input id="" type="text" name="release_name" value="{{ old('release_name') }}">
                     @error('release_name')
@@ -47,7 +44,7 @@
                     @enderror
                 </div>
 
-                <div class="field">
+                <div class="field field-alt">
                     <label for="edition_name">{{ __('Editie') }}</label>
                    <input id="" type="text" name="edition_name" value="{{ old('edition_name') }}">
                     @error('edition_name')
@@ -57,7 +54,7 @@
                     @enderror
                 </div>
 
-                <div class="field">
+                <div class="field field-alt">
                     <label for="print_edition">{{ __('Oplage') }}</label>
                    <input id="" type="text" name="print_edition" value="{{ old('print_edition') }}">
                     @error('print_edition')
@@ -70,67 +67,82 @@
             </fieldset>
 
             <fieldset class="fields page-details">
-                <h3>{{ __('Pagina gegevens') }}</h3>
-                <div class="form__row">
-                    <div class="field">
-                        <label for="pages_redaction">{{ __('Redactie') }}</label>
-                        <input id="pages_redaction" type="number" name="pages_redaction" value="{{ old('pages_redaction') }}">
-                        @error('pages_redaction')
-                            <span class="form__message" role="alert">
-                                <small>{{ $message }}</small>
-                            </span>
-                        @enderror
-                    </div>
+                <h3>{{ __('Paginagegevens') }}</h3>
+                <div class="form__row-alt">
+                    <label>Aantal pagina's</label>
+                    <div class="field__row">
+                        <div class="form__row">
+                            <div class="field">
+                                <label for="pages_redaction">{{ __('Redactie') }}</label>
+                                <input id="pages_redaction" type="number" name="pages_redaction" value="{{ old('pages_redaction') }}">
+                                @error('pages_redaction')
+                                    <span class="form__message" role="alert">
+                                        <small>{{ $message }}</small>
+                                    </span>
+                                @enderror
+                            </div>
 
-                    <div class="field">
-                        <label for="pages_adverts">{{ __('Advertenties') }}</label>
-                        <input id="pages_adverts" type="number" name="pages_adverts" value="{{ old('pages_adverts') }}">
-                        @error('pages_adverts')
-                            <span class="form__message" role="alert">
-                                <small>{{ $message }}</small>
-                            </span>
-                        @enderror
+                            <div class="field">
+                                <label for="pages_adverts">{{ __('Advertenties') }}</label>
+                                <input id="pages_adverts" type="number" name="pages_adverts" value="{{ old('pages_adverts') }}">
+                                @error('pages_adverts')
+                                    <span class="form__message" role="alert">
+                                        <small>{{ $message }}</small>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
                     </div>
                 </div>
                 <p name="total_pages" id="sum">Totaal aantal pagina's:</p>
-                <div class="form__row">
-                    <div class="field">
-                        <label for="paper_type_cover">{{ __('Papier cover') }}</label>
-                        <input id="" type="text" name="paper_type_cover" value="{{ old('paper_type_cover') }}">
-                        @error('paper_type_cover')
-                            <span class="form__message" role="alert">
-                                <small>{{ $message }}</small>
-                            </span>
-                        @enderror
-                    </div>
-                    <div class="field">
-                        <label for="color_cover">{{ __('Kleurgebruik cover') }}</label>
-                        <input id="" type="text" name="color_cover" value="{{ old('color_cover') }}">
-                        @error('color_cover')
-                            <span class="form__message" role="alert">
-                                <small>{{ $message }}</small>
-                            </span>
-                        @enderror
+                <div class="form__row-alt">
+                    <label>Cover</label>
+                    <div class="field__row">
+                        <div class="form__row">
+                            <div class="field">
+                                <label for="paper_type_cover">{{ __('Papier cover') }}</label>
+                                <input id="" type="text" name="paper_type_cover" value="{{ old('paper_type_cover') }}">
+                                @error('paper_type_cover')
+                                    <span class="form__message" role="alert">
+                                        <small>{{ $message }}</small>
+                                    </span>
+                                @enderror
+                            </div>
+                            <div class="field">
+                                <label for="color_cover">{{ __('Kleurgebruik cover') }}</label>
+                                <input id="" type="text" name="color_cover" value="{{ old('color_cover') }}">
+                                @error('color_cover')
+                                    <span class="form__message" role="alert">
+                                        <small>{{ $message }}</small>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
                     </div>
                 </div>
-                <div class="form__row">
-                    <div class="field">
-                        <label for="paper_type_interior">{{ __('Papier binnenkant') }}</label>
-                        <input id="" type="text" name="paper_type_interior" value="{{ old('paper_type_interior') }}">
-                        @error('paper_type_interior')
-                            <span class="form__message" role="alert">
-                                <small>{{ $message }}</small>
-                            </span>
-                        @enderror
-                    </div>
-                    <div class="field">
-                        <label for="color_interior">{{ __('Kleurgebruik binnenkant') }}</label>
-                        <input id="" type="text" name="color_interior" value="{{ old('color_interior') }}">
-                        @error('color_interior')
-                            <span class="form__message" role="alert">
-                                <small>{{ $message }}</small>
-                            </span>
-                        @enderror
+                <div class="form__row-alt">
+                    <label>Binnenwerk</label>
+                    <div class="field__row">
+                        <div class="form__row">
+                            <div class="field">
+                                <label for="paper_type_interior">{{ __('Papier binnenkant') }}</label>
+                                <input id="" type="text" name="paper_type_interior" value="{{ old('paper_type_interior') }}">
+                                @error('paper_type_interior')
+                                    <span class="form__message" role="alert">
+                                        <small>{{ $message }}</small>
+                                    </span>
+                                @enderror
+                            </div>
+                            <div class="field">
+                                <label for="color_interior">{{ __('Kleurgebruik binnenkant') }}</label>
+                                <input id="" type="text" name="color_interior" value="{{ old('color_interior') }}">
+                                @error('color_interior')
+                                    <span class="form__message" role="alert">
+                                        <small>{{ $message }}</small>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
                     </div>
                 </div>
             </fieldset>
@@ -138,7 +150,7 @@
             <fieldset class="fields designer'">
                 <h3>{{ __('Vormgeving / drukker') }}</h3>
 
-                <div class="field">
+                <div class="field field-alt">
                     <label for="designer">{{ __('Vormgever') }}</label>
                     <input id="" type="text" name="designer" value="{{ old('designer') }}">
                     @error('designer')
@@ -148,7 +160,7 @@
                     @enderror
                 </div>
 
-                <div class="field">
+                <div class="field field-alt">
                     <label for="printer">{{ __('Drukker') }}</label>
                     <input id="" type="text" name="printer" value="{{ old('printer') }}">
                     @error('printer')
@@ -158,7 +170,7 @@
                     @enderror
                 </div>
 
-                <div class="field">
+                <div class="field field-alt">
                     <label for="client">{{ __('Opdrachtgever') }}</label>
                     <input id="" type="text" name="client" value="{{ old('client') }}">
                     @error('client')
@@ -168,7 +180,7 @@
                     @enderror
                 </div>
 
-                <div class="field">
+                <div class="field field-alt">
                     <label for="distribution">{{ __('Verspreider') }}</label>
                     <input id="" type="text" name="distribution" value="{{ old('distribution') }}">
                     @error('distribution')
@@ -183,7 +195,8 @@
             <fieldset class="fields financial'">
                 <h3>{{ __('Financieel') }}</h3>
                 
-                <div class="field">
+                {{--
+                    <div class="field field-alt">
                     <label for="layout">{{ __('Layout') }}</label>
                     <div class="dropdown">
                         <select title="layout" name="layout" id="layout">
@@ -197,8 +210,9 @@
                         </select>
                     </div>
                 </div>
+                --}}
 
-                <div class="field">
+                <div class="field field-alt">
                     <label for="taxes">{{ __('BTW') }}</label>
                     <div class="dropdown">
                         <select title="taxes" name="taxes" id="taxes">
@@ -213,7 +227,7 @@
                     </div>
                 </div>
 
-                <div class="field">
+                <div class="field field-alt">
                     <label for="ledger">{{ __('Grootboek') }}</label>
                     <input id="" type="text" name="ledger" value="{{ old('ledger') }}">
                     @error('ledger')
@@ -223,7 +237,7 @@
                     @enderror
                 </div>
 
-                <div class="field">
+                <div class="field field-alt">
                     <label for="journal">{{ __('Dagboek') }}</label>
                     <input id="" type="text" name="journal" value="{{ old('journal') }}">
                     @error('journal')
@@ -233,7 +247,7 @@
                     @enderror
                 </div>
 
-                <div class="field">
+                <div class="field field-alt">
                     <label for="department">{{ __('Kostenplaats') }}</label>
                     <input id="" type="text" name="department" value="{{ old('department') }}">
                     @error('department')
@@ -243,7 +257,7 @@
                     @enderror
                 </div>
 
-                <div class="field">
+                <div class="field field-alt">
                     <label for="year">{{ __('Jaar') }}</label>
                     <input id="" type="text" name="year" value="{{ old('year') }}">
                     @error('year')
@@ -253,7 +267,7 @@
                     @enderror
                 </div>
 
-                <div class="field">
+                <div class="field field-alt">
                     <label for="revenue_goals">{{ __('Omzetdoelstelling') }}</label>
                     <input id="" type="text" name="revenue_goals" value="{{ old('revenue_goals') }}">
                     @error('revenue_goals')
@@ -266,23 +280,22 @@
             </fieldset>
 
             <fieldset class="field notes">
-
-            <label for="comments">{{ __('Opmerkingen') }}</label>
-            <textarea id="" cols="30" rows="10" name="comments" value="{{ old('comments') }}" placeholder="Vul opmerkingen in..."></textarea>
-            @error('comments')
-                <span class="form__message" role="alert">
-                    <small>{{ $message }}</small>
-                </span>
-            @enderror
+                <label for="comments">{{ __('Opmerkingen') }}</label>
+                <textarea id="" cols="30" rows="10" name="comments" value="{{ old('comments') }}" placeholder="Vul opmerkingen in..."></textarea>
+                @error('comments')
+                    <span class="form__message" role="alert">
+                        <small>{{ $message }}</small>
+                    </span>
+                @enderror
             </fieldset>
         </div>
 
-        <div class="formBlock">
-            
-    
-            
-
-            <button type="submit">Toevoegen</button>
+        <div class="ButtonGroup">
+            <div class="buttons">
+                <button type="submit" class="button button--action">
+                    {{ __('Toevoegen') }}
+                </button>
+            </div>
         </div>
     </form>
 
