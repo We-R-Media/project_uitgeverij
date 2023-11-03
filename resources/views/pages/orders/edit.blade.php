0ee4f89347ba7a822d4f29c616e293e57ead2b77
Mistrcrx <84770090+Mistrcrx@users.noreply.github.com>
2023-11-03 11:36:26 +0100

    Merged with new routes from Jeroen

--- resources/views/pages/orders/edit.blade.php
+++ resources/views/pages/orders/edit.blade.php
@@ -4,71 +4,108 @@
 
 @section('content')
 
-
+<div class="page__wrapper">
     <form  class="formContainer" action="{{route('orders.create')}}" method="post">
-        <div class="formBlock">
-            <h3>Bevestigingsadres</h3>
+        <div class="grid__wrapper">
+            <fieldset class="fields base">
+                <h3>{{ __('Bevestigingsadres') }}</h3>
 
-            <input type="text" class="@error ('advertiser') is-invalid @enderror" value="{{ old('advertiser') }}" name="advertiser" placeholder="Vul klantnummer in..." id="">
-                @if($errors->has('advertiser'))
-                    <p class="error-message">{{$errors->first('advertiser')}}</p>
-                @endif
-            <input type="text" class="@error ('company') is-invalid @enderror" value="{{ old('company') }}" name="company" placeholder="Vul bedrijfsnaam in..." id="">
-                @if($errors->has('company'))
-                    <p class="error-message">{{$errors->first('company')}}</p>
-                @endif
+                <div class="field field-alt">
+                    <label for="advertiser">{{ __('Klantnummer') }}</label>
+                    <input id="" type="text" name="advertiser" value="{{ old('advertiser') }}">
+                    @error('advertiser')
+                        <span class="form__message" role="alert">
+                            <small>{{ $message }}</small>
+                        </span>
+                    @enderror
+                </div>
 
-            <input type="text" class="@error ('contact') is-invalid @enderror" value="{{ old('contact') }}" name="contact" placeholder="Vul contact in..." id="">
-                @if($errors->has('contact'))
-                    <p class="error-message">{{$errors->first('contact')}}</p>
-                @endif
+                <div class="field field-alt">
+                    <label for="company">{{ __('Bedrijfsnaam') }}</label>
+                    <input id="" type="text" name="company" value="{{ old('company') }}">
+                    @error('company')
+                        <span class="form__message" role="alert">
+                            <small>{{ $message }}</small>
+                        </span>
+                    @enderror
+                </div>
 
-            <input type="text" class="@error ('po_box') is-invalid @enderror" value="{{ old('po_box') }}" name="po_box" placeholder="Vul postadres in..." id="">
-                @if($errors->has('po_box'))
-                    <p class="error-message">{{$errors->first('po_box')}}</p>
-                @endif
+                <div class="field field-alt">
+                    <label for="contact">{{ __('Contactpersoon') }}</label>
+                    <input id="" type="text" name="contact" value="{{ old('contact') }}">
+                    @error('contact')
+                        <span class="form__message" role="alert">
+                            <small>{{ $message }}</small>
+                        </span>
+                    @enderror
+                </div>
 
-            <input type="text" class="@error ('postal_code') is-invalid @enderror" value="{{ old('postal_code') }}" name="postal_code" placeholder="Vul postcode in..." id="">
-                @if($errors->has('postal_code'))
-                    <p class="error-message">{{$errors->first('postal_code')}}</p>
-                @endif
+                <div class="field field-alt">
+                    <label for="po_box">{{ __('Postadres') }}</label>
+                    <input id="" type="text" name="po_box" value="{{ old('po_box') }}">
+                    @error('po_box')
+                        <span class="form__message" role="alert">
+                            <small>{{ $message }}</small>
+                        </span>
+                    @enderror
+                </div>
 
-            <input type="text" class="@error ('city') is-invalid @enderror" value="{{ old('city') }}" name="city" placeholder="Vul plaats in..." id="">
-                @if($errors->has('city'))
-                    <p class="error-message">{{$errors->first('city')}}</p>
-                @endif
+                <div class="field field-alt">
+                    <label for="postal_code">{{ __('Postcode') }}</label>
+                    <input id="" type="text" name="postal_code" value="{{ old('postal_code') }}">
+                    @error('postal_code')
+                        <span class="form__message" role="alert">
+                            <small>{{ $message }}</small>
+                        </span>
+                    @enderror
+                </div>
 
+                <div class="field field-alt">
+                    <label for="city">{{ __('Woonplaats') }}</label>
+                    <input id="" type="text" name="city" value="{{ old('city') }}">
+                    @error('city')
+                        <span class="form__message" role="alert">
+                            <small>{{ $message }}</small>
+                        </span>
+                    @enderror
+                </div>
 
-            <input type="text" class="@error ('province') is-invalid @enderror" value="{{ old('province') }}" name="province" placeholder="Vul provincie in..." id="">
-                @if($errors->has('province'))
-                    <p class="error-message">{{$errors->first('province')}}</p>
-                @endif
+                <div class="field field-alt">
+                    <label for="province">{{ __('Provincie') }}</label>
+                    <input id="" type="text" name="province" value="{{ old('province') }}">
+                    @error('province')
+                        <span class="form__message" role="alert">
+                            <small>{{ $message }}</small>
+                        </span>
+                    @enderror
+                </div>
 
-            <input type="text" class="@error ('country') is-invalid @enderror" value="{{ old('country') }}" name="country" placeholder="Vul land in..." id="">
-                @if($errors->has('country'))
-                    <p class="error-message">{{$errors->first('country')}}</p>
-                @endif
+                <div class="field field-alt">
+                    <label for="country">{{ __('Land') }}</label>
+                    <input id="" type="text" name="country" value="{{ old('country') }}">
+                    @error('country')
+                        <span class="form__message" role="alert">
+                            <small>{{ $message }}</small>
+                        </span>
+                    @enderror
+                </div>
 
-            <input type="email" class="@error ('email') is-invalid @enderror" value="{{ old('email') }}" name="email" placeholder="Vul email in..." id="">
-                @if($errors->has('email'))
-                    <p class="error-message">{{$errors->first('email')}}</p>
-                @endif
+                <div class="field field-alt">
+                    <label for="email">{{ __('E-mailadres') }}</label>
+                    <input id="" type="email" name="email" value="{{ old('email') }}">
+                    @error('email')
+                        <span class="form__message" role="alert">
+                            <small>{{ $message }}</small>
+                        </span>
+                    @enderror
+                </div>
+            </fieldset>
         </div>
-    {{--
-        <div class="formBlock">
-            <h3>Afwijkend factuuradres</h3>
-            <input type="text" name="advertiser" placeholder="Vul klantnummer in..." id="">
-            <input type="text" name="company" placeholder="Vul bedrijfsnaam in..." id="">
-            <input type="text" name="contact" placeholder="Vul contact in..." id="">
-            <input type="text" name="mailbox" placeholder="Vul postadres in..." id="">
-            <input type="text" name="postal_code" placeholder="Vul postcode in..." id="">
-            <input type="text" name="province" placeholder="Vul provincie in..." id="">
-            <input type="text" name="country" placeholder="Vul land in..." id="">
-            <input type="email" name="email" placeholder="Vul email in..." id="">
-        </div> --}}
-
+        <div class="ButtonGroup">
+            <div class="buttons">
+                <a href="{{route('pdf.generate')}}" class="button button--action">{{ __('Genereer PDF') }}</a>
+            </div>
+        </div>
     </form>
-
-    <a href="{{route('pdf.generate')}}">Genereer PDF</a>
-
+</div>
 @endsection
